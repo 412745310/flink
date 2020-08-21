c2e0fa6960d58f2271c893fd1223e80f976fd1b7
baojun_a <baojun_a@aspirecn.com>
2020-08-21 14:56:37 +0800

    新增数据源代码示例

---
+++ flink-batch/src/main/java/com/chelsea/flink/batch/DataSourceDemo.java
@@ -0,0 +1,20 @@
+package com.chelsea.flink.batch;
+
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+
+/**
+ * 数据源代码示例
+ * 
+ * @author shevchenko
+ *
+ */
+public class DataSourceDemo {
+    
+    public static void main(String[] args) throws Exception {
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        DataSet<String> ds = env.readTextFile("hdfs://172.18.20.237:9000/flink/batch/t_t1.txt");
+        ds.print();
+    }
+
+}
