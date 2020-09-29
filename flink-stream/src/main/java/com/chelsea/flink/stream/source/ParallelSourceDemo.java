4ca3629220e21d4c3159ad679c3ef71a3d254ce7
baojun_a <baojun_a@aspirecn.com>
2020-09-29 10:33:50 +0800

    修改类名

--- flink-stream/src/main/java/com/chelsea/flink/stream/source/ParallelSourceDemo.java
+++ flink-stream/src/main/java/com/chelsea/flink/stream/source/ParallelSourceDemo.java
@@ -1,29 +1,41 @@
 package com.chelsea.flink.stream.source;
 
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.source.SourceFunction;
+import org.apache.flink.streaming.api.functions.source.RichParallelSourceFunction;
 
 /**
- * 自定义非并行数据源
+ * 自定义并行数据源
  * 
  * @author shevchenko
  *
  */
-public class SourceFunctionDemo {
+public class ParallelSourceDemo {
 
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        DataStreamSource<Long> addSource = env.addSource(new MyNoParalleSourceFunction());
+        // 设置并行度为3，意味着有3个task线程同时在执行source源的run方法
+        DataStreamSource<Long> addSource = env.addSource(new MyParalleSourceFunction()).setParallelism(3);
         addSource.print();
         env.execute();
     }
     
-    static class MyNoParalleSourceFunction implements SourceFunction<Long> {
+    static class MyParalleSourceFunction extends RichParallelSourceFunction<Long> {
 
         private static final long serialVersionUID = 1L;
         private boolean isRunning = true;
         private Long count = 0l;
+        
+        @Override
+        public void open(Configuration parameters) throws Exception {
+            System.out.println("初始化方法，每个task线程只执行一次");
+        }
+        
+        @Override
+        public void close() throws Exception {
+            System.out.println("资源回收方法，每个task线程只执行一次");
+        }
 
         @Override
         public void cancel() {
