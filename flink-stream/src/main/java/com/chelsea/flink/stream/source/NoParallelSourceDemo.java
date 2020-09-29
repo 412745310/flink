b3e6b2de387be2cf7efdfb6a316cbc485cce867c
baojun_a <baojun_a@aspirecn.com>
2020-09-29 10:34:05 +0800

    修改类名

---
+++ flink-stream/src/main/java/com/chelsea/flink/stream/source/NoParallelSourceDemo.java
@@ -0,0 +1,45 @@
+package com.chelsea.flink.stream.source;
+
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+
+/**
+ * 自定义非并行数据源
+ * 
+ * @author shevchenko
+ *
+ */
+public class NoParallelSourceDemo {
+
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        // 不能设置大于1的并行度，否则报错
+        DataStreamSource<Long> addSource = env.addSource(new MyNoParalleSourceFunction());
+        addSource.print();
+        env.execute();
+    }
+    
+    static class MyNoParalleSourceFunction implements SourceFunction<Long> {
+
+        private static final long serialVersionUID = 1L;
+        private boolean isRunning = true;
+        private Long count = 0l;
+
+        @Override
+        public void cancel() {
+            isRunning = false;
+        }
+
+        @Override
+        public void run(SourceContext<Long> out) throws Exception {
+            while(isRunning) {
+                count++;
+                out.collect(count);
+                Thread.sleep(1000);
+            }
+        }
+        
+    }
+
+}
