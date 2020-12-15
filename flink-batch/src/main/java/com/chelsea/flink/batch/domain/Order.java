582c557055643f0a4deb00c1d1d444f6174f8684
baojun_a <baojun_a@aspirecn.com>
2020-12-15 15:17:41 +0800

    新增实体类

---
+++ flink-batch/src/main/java/com/chelsea/flink/batch/domain/Order.java
@@ -0,0 +1,14 @@
+package com.chelsea.flink.batch.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    private Integer id;
+    private String userName;
+    private Double money;
+}
